ff5c0a23790329d1c95add2e51f2bffa9846a934
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2023-02-01 10:36:37 +0000

    [finish 3]

--- app/controllers/dos_controller.rb
+++ app/controllers/dos_controller.rb
@@ -1,54 +1,54 @@
 class DosController < ApplicationController
-  before_action :set_do, only: [:show, :edit, :update, :destroy]
-
-  # GET /dos
-  def index
-    @dos = Do.all
-  end
-
-  # GET /dos/1
-  def show
-  end
+  before_action :authenticate_user!
+  before_action :set_goal
+  before_action :set_do, only: [:show, :edit, :update, :destroy, :sort]
 
   # GET /dos/new
   def new
-    @do = Do.new
+    @do = @goal.dos.new
   end
 
   # GET /dos/1/edit
   def edit
   end
 
+
+  def sort
+  end
   # POST /dos
   def create
-    @do = Do.new(do_params)
+    @do = @goal.dos.new(do_params)
 
     if @do.save
-      redirect_to @do, notice: 'Do was successfully created.'
+      @status = true
     else
-      render :new
+      @status = false
     end
   end
 
   # PATCH/PUT /dos/1
   def update
     if @do.update(do_params)
-      redirect_to @do, notice: 'Do was successfully updated.'
+      @status = true
     else
-      render :edit
+      @status = false
     end
   end
 
   # DELETE /dos/1
   def destroy
     @do.destroy
-    redirect_to dos_url, notice: 'Do was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
+    def set_goal
+      @goal = current_user.goals.find_by(id: params[:goal_id])
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
+    end
+    
     def set_do
-      @do = Do.find(params[:id])
+      @do = @goal.dos.find_by(id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  has_many :dos, dependent: :destroy
+  
+  validates :title, presence: true
 end
